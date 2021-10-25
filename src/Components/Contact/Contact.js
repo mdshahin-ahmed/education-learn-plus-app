f7910ef6e50374caff985551f4a556eb1c1c1f8d
mdshahin-ahmed <mdshahinahmed605701@gmail.com>
2021-10-25 23:39:56 +0600

    Done my Assignment 9

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,10 +1,27 @@
+import Button from '@restart/ui/esm/Button';
 import React from 'react';
+import { Form } from 'react-bootstrap';
+import './Contact.css';
 
 const Contact = () => {
     return (
-        <div>
-            
-        </div>
+        <Form className='form-style'>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter Your Name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter Your Email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea3">
+            <Form.Label>Message</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+        <Button className='btn btn-primary' variant="primary" type="submit">
+            Submit
+        </Button>
+        </Form>
     );
 };
 
